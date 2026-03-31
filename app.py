5099d04dce0bedb0b21040b04b43d4d1b10a14e1
Camilosorio98 <camilo.pineda.o@gmail.com>
2026-03-30 22:26:15 -0500

    Add Extra Trees Classifier module and update navigation menu

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app.controllers.spending_controller import spending_bp
 from app.controllers.demand_controller import demand_bp
 from app.controllers.linear_regression_controller import linear_regression_bp
 from app.controllers.logistic_regression_controller import logistic_regression_bp
+from app.controllers.extra_trees_controller import extra_trees_bp  # NUEVO CONTROLADOR
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
@@ -14,6 +15,7 @@ app.register_blueprint(spending_bp)
 app.register_blueprint(demand_bp)
 app.register_blueprint(linear_regression_bp)
 app.register_blueprint(logistic_regression_bp)
+app.register_blueprint(extra_trees_bp)  # NUEVO REGISTRO
 
 @app.route('/')
 def home():
